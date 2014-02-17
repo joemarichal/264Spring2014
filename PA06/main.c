6e04db83b36c3660032f4139bf9ce42d4c144126
Joe <joe@joe-VirtualBox.(none)>
2014-02-17 18:51:05 -0500

    First draft each function

---
+++ PA06/main.c
@@ -0,0 +1,20 @@
+#include "answer06.h"
+#include <stdio.h>
+
+int main(void)
+{
+
+  printf("Partition Increasing 7\n");
+  partitionIncreasing(7);
+  printf("Partition Decreasing 7\n");
+  partitionDecreasing(7);
+  printf("Partition Even 6\n");
+  partitionEven(6);
+  printf("Partition Odd 6\n");
+  partitionOdd(6);
+  printf("Even and Odd 6\n");
+  partitionOddAndEven(6);
+  printf("Prime 10\n");
+  partitionPrime(10);
+  return 0;
+}
